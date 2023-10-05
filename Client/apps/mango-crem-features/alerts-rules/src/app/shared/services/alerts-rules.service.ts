53770f337d387cfe0f6842a2d2b00436a730071d
Abderraouf Safsaf <asafsaf@costargroup.com>
2023-10-05 13:55:10 -0400

    Merged PR 11514: Fix batch accounting
    Related work items: #129217

--- Client/apps/mango-crem-features/alerts-rules/src/app/shared/services/alerts-rules.service.ts
+++ Client/apps/mango-crem-features/alerts-rules/src/app/shared/services/alerts-rules.service.ts
@@ -1,153 +1,46 @@
-/* eslint-disable no-prototype-builtins */
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { environment } from '../../../../../../mango/src/environments/environment.local';
-import { AlertRuleUpdate, ApiResponse } from '../models';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type HttpParamsObj = HttpParams | { [param: string]: any };
-type HeadersObj = {
-  headers: HttpHeaders,
-  params?: HttpParamsObj
-};
+import { EndpointService } from '@mango/core-shared/lib-core-shared';
+import { environment } from '@mangoSpa/src/environments/environment.local';
+import { AlertRuleUpdate } from '../models';
 
 @Injectable({ providedIn: 'root' })
-export class AlertsRulesService {
-  private apiUrl: string;
-
-  private httpOptions: HeadersObj = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      UserId: '2',
-      ClientKey: 'RETAILDEMO',
-      CAEnabled: 'false'
-    })
-  };
-
-  constructor(private http: HttpClient) {
-    this.apiUrl = environment.appUrls.alertsRules;
-  }
+export class AlertsRulesService extends EndpointService{
 
   getUserModuleRights() {
-    return this.callHttpGet(`${this.apiUrl}/GetUserModuleRights`, 'getUserModuleRights');
+    return this.callHttpGet(`${environment.appUrls.alertsRules}/GetUserModuleRights`, 'getUserModuleRights');
   }
 
   getAlertTypes() {
-    return this.callHttpGet(`${this.apiUrl}/GetAlertTypes`, 'getAlertTypes');
+    return this.callHttpGet(`${environment.appUrls.alertsRules}/GetAlertTypes`, 'getAlertTypes');
   }
 
   getAlertRuleSeverities() {
     return this.callHttpGet(
-      `${this.apiUrl}/GetAlertRuleSeverities`, 'getAlertRuleSeverities'
+      `${environment.appUrls.alertsRules}/GetAlertRuleSeverities`, 'getAlertRuleSeverities'
     );
   }
 
   getAlertRules(OTID: number) {
-    if (environment.isRestful) {
-      return this.callHttpGet(`${this.apiUrl}/GetAlertRules/${OTID}`, 'getAlertRules');
-    }
-
-    return this.callHttpGet(
-      `${this.apiUrl}/GetAlertRules`, 'getAlertRules', { objectTypeId: OTID }
-    );
+      return this.callHttpGet(`${environment.appUrls.alertsRules}/GetAlertRules/${OTID}`, 'getAlertRules');
   }
 
   updateAlertRules(rules: AlertRuleUpdate[]) {
-    if (environment.isRestful) {
       return this.callHttpPost(
-        `${this.apiUrl}/UpdateAlertRules`, 'updateAlertRules', JSON.stringify(rules)
+        `${environment.appUrls.alertsRules}/UpdateAlertRules`, 'updateAlertRules', JSON.stringify(rules)
       );
-    }
-
-    return this.callHttpPost(
-      `${this.apiUrl}/UpdateAlertRules`, 'updateAlertRules',
-      JSON.stringify({ ruleUpdates: rules })
-    );
   }
 
   getIsAlertDismissedReasonRequired(objectTypeId: number) {
-    if (environment.isRestful) {
       return this.callHttpGet(
-        `${this.apiUrl}/IsAlertDismissedReasonRequired/ObjectType/${objectTypeId}`,
+        `${environment.appUrls.alertsRules}/IsAlertDismissedReasonRequired/ObjectType/${objectTypeId}`,
         'isDismissReasonRequired'
       );
-    }
-
-    return this.callHttpGet(
-      `${this.apiUrl}/IsAlertDismissedReasonRequired`, 'isDismissReasonRequired', { objectTypeId }
-    );
   }
 
   toggleAlertDismissedReasonIsRequired(objectTypeId: number) {
-    if(environment.isRestful) {
       return this.callHttpGet(
-        `${this.apiUrl}/ToggleAlertDismissedReasonIsRequired/ObjectType/${objectTypeId}`,
+        `${environment.appUrls.alertsRules}/ToggleAlertDismissedReasonIsRequired/ObjectType/${objectTypeId}`,
         'toggleReasonRequired'
       );
-    }
-
-    return this.callHttpGet(
-        `${this.apiUrl}/ToggleAlertDismissedReasonIsRequired`, 'toggleReasonRequired', { objectTypeId }
-    );
-  }
-
-  protected callHttpGet(url: string, logName: string, httpOptionsParams?: HttpParamsObj) {
-    if (httpOptionsParams) {
-      this.httpOptions.params = httpOptionsParams;
-    }
-
-    return this.http.get(url, this.httpOptions).pipe(
-      map(x => this.toApiResponse(x)),
-      catchError(this.handleError(logName))
-    );
-  }
-
-  protected callHttpPost(url: string, logName: string, postBody: string) {
-    return this.http.post(url, postBody, this.httpOptions).pipe(
-      map(x => this.toApiResponse(x)),
-      catchError(this.handleError(logName))
-    );
-  }
-
-  private handleError(logName: string = 'Operation not provided') {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return (error: any): Observable<ApiResponse> => {
-      console.error(logName, error);
-
-      return of({
-        succeeded: false,
-        message: error.statusText,
-        data: []
-      });
-    };
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private toApiResponse(value: any): ApiResponse {
-    const val = value.hasOwnProperty('d')
-      ? value.d
-      : value;
-
-    const res = val.hasOwnProperty('Result')
-      ? JSON.parse(val.Result)
-      : val;
-
-    const data = res.hasOwnProperty('data')
-      ? (res.data instanceof String)
-        ? JSON.parse(res.data)
-        : res.data
-      : res;
-
-    return {
-      succeeded: res.succeeded,
-      message: res.message,
-      data: data.hasOwnProperty('data') ? data.data : data
-    }
   }
 }
