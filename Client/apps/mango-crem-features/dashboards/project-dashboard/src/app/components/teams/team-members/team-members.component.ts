edcfb7b588f648ebedfec909d0db005e7a0c0243
Aruna Ningampally <aningampally@costargroup.com>
2023-10-25 15:59:30 -0400

    Merged PR 11802: 130174 130335 Team Members Subgrid layout changes
    130174 130335 Team Members Subgrid layout changes
    
    Related work items: #130174, #130335

--- Client/apps/mango-crem-features/dashboards/project-dashboard/src/app/components/teams/team-members/team-members.component.ts
+++ Client/apps/mango-crem-features/dashboards/project-dashboard/src/app/components/teams/team-members/team-members.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Output, EventEmitter } from '@angular/core';
 import { TeamMember } from '@mango/data-models/lib-data-models';
 import { DxDataGridComponent } from 'devextreme-angular';
+import CheckBox from 'devextreme/ui/check_box';
 
 
 @Component({
@@ -13,14 +14,21 @@ export class TeamMembersComponent implements OnInit {
 	@Input() teamMembers : TeamMember[];
 	@Input() searchText: string;
 	@Input() rights: string;
+	@Input() teamId: number;
+
 	public dataRetrieved: boolean = false;
+	saveTeamMember: TeamMember;
+	isEdit: boolean = false;
+	headerCheckBox: any;
+	headerHtmlCellElement: any;
+	editDataRow: any
 
 	@ViewChild("TeamMembersGrid") teamMembersGrid: DxDataGridComponent;
+	@Output() subGridEditClicked: EventEmitter<any> = new EventEmitter();
 
 	constructor() {}	
 
-	ngOnInit() {
-	}
+	ngOnInit() {}
 
 	ngAfterViewInit() {
     if(this.searchText) this.searchDataGrid();
@@ -28,6 +36,46 @@ export class TeamMembersComponent implements OnInit {
 
 	searchDataGrid() {
 		this.teamMembersGrid.instance.searchByText(this.searchText);
+	}
+
+	editRow(row) {
+		this.editDataRow = row;
+		//this.subGridEditClicked.emit(this.teamId);
+		this.teamMembers.forEach(teamMember => { teamMember.editMode = false});
+		this.teamMembersGrid.instance.cancelEditData();
+		this.teamMembersGrid.instance.editRow(row.rowIndex);
+		this.saveTeamMember = row.data
+		this.isEdit = true;
+		row.data.editMode = true;
+	}
+
+	gridOnCellPrepared(e) {
+		if(this.rights.toLocaleLowerCase().trim()=="view") {
+			if( e.rowType == 'header' && e.column.command == 'select') {
+				this.headerHtmlCellElement = e.cellElement.length === undefined ? e.cellElement : e.cellElement[0];   
+				this.headerCheckBox = CheckBox.getInstance(this.headerHtmlCellElement.querySelector(".dx-select-checkbox")); 
+				if(!(!this.headerCheckBox))
+				this.headerCheckBox.option("disabled", true);  
+				if(this.headerHtmlCellElement) {
+					this.headerHtmlCellElement.style.pointerEvents = 'none';
+				}
+			}
+			let htmlCellElement = e.cellElement.length === undefined ? e.cellElement : e.cellElement[0];   
+			var editor = CheckBox.getInstance(htmlCellElement.querySelector(".dx-select-checkbox"));  
+			if(editor) {
+				editor.option("disabled", true);
+			}  
+			htmlCellElement.style.pointerEvents = 'none'; 
+		}  
+	}
+
+	saveChanges(row) {
+
+	}
+	
+	cancelChanges(row) {
+		row.data.editMode = false;
+		this.teamMembersGrid.instance.cancelEditData();
 	}
 
 	doSomethingForNow(data) {}
--- Client/apps/mango-crem-features/dashboards/project-dashboard/src/app/components/teams/teams.component.ts
+++ Client/apps/mango-crem-features/dashboards/project-dashboard/src/app/components/teams/teams.component.ts
@@ -5,6 +5,8 @@ import { DashboardService } from '@project-dashboard/services/dashboard.service'
 import { Router } from '@angular/router';
 import { CardsService } from '@project-dashboard/services/cards.service';
 import { Team, TeamMember} from '@mango/data-models/lib-data-models';
+import { TeamMembersComponent } from './team-members/team-members.component';
+import CheckBox from 'devextreme/ui/check_box';
 
 @Component({
   selector: 'teams',
@@ -14,12 +16,15 @@ import { Team, TeamMember} from '@mango/data-models/lib-data-models';
 export class TeamsComponent implements OnInit {
   @ViewChild("TeamsGrid") teamsGrid: DxDataGridComponent;
   @ViewChild('SearchBox') searchBox: SearchComponent;
+  @ViewChild(TeamMembersComponent) teamMembersComponent: TeamMembersComponent;
 
   searchText: string = "";
   teams: Team[];
   teamMembers: TeamMember[];
   dataRetrieved: boolean = false;
   autoExpand: boolean = false;
+  headerCheckBox: any;
+	headerHtmlCellElement: any;
 
   constructor(private dashboardService: DashboardService, private router: Router,
               private cardsService: CardsService) { }
@@ -32,12 +37,23 @@ export class TeamsComponent implements OnInit {
       (res:any) => {
         this.teams = res.data;
         this.dataRetrieved = true;
+        this.initEditFlag(this.teams);
       },
       (error: any) => console.log("Error occurred getting Teams Data ", error),
       () => {}
     );
   }
   
+  initEditFlag(teams) {
+    if(teams.length) {
+      teams.forEach(team => {
+        if(team.teamMembers.length) {
+          team.teamMembers.forEach(teamMember => { teamMember.editMode = false})
+        }
+      })
+    }
+  }
+
   addTeam() {}
 
   doSomethingForNow(data) {}
@@ -58,7 +74,7 @@ export class TeamsComponent implements OnInit {
 
   searchDataGrid(data) {
     this.searchText = data;
-		this.teamsGrid.instance.searchByText(data);
+		this.teamsGrid.instance.searchByText(this.searchText);
 	}
 
   exportDataGrid(): void {
