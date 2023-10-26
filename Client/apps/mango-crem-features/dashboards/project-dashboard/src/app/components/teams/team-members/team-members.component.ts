5eaf7a56d3f45aced1c9adedbf858a624b9dcaa4
Aruna Ningampally <aningampally@costargroup.com>
2023-10-26 15:12:06 -0400

    Merged PR 11825: 130335 - Team Members SubGrid remove team member changes
    130335 - Team Members SubGrid remove team member changes
    
    Related work items: #130335

--- Client/apps/mango-crem-features/dashboards/project-dashboard/src/app/components/teams/team-members/team-members.component.ts
+++ Client/apps/mango-crem-features/dashboards/project-dashboard/src/app/components/teams/team-members/team-members.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ViewChild, Output, EventEmitter } from '@angular/core';
 import { TeamMember } from '@mango/data-models/lib-data-models';
 import { DxDataGridComponent } from 'devextreme-angular';
+import { DashboardService } from '@project-dashboard/services/dashboard.service';
 import CheckBox from 'devextreme/ui/check_box';
 
 
@@ -17,16 +18,16 @@ export class TeamMembersComponent implements OnInit {
 	@Input() teamId: number;
 
 	public dataRetrieved: boolean = false;
-	saveTeamMember: TeamMember;
-	isEdit: boolean = false;
+	memberIds: number[];
 	headerCheckBox: any;
 	headerHtmlCellElement: any;
 	editDataRow: any
+	emailNotify: boolean;
 
 	@ViewChild("TeamMembersGrid") teamMembersGrid: DxDataGridComponent;
 	@Output() subGridEditClicked: EventEmitter<any> = new EventEmitter();
 
-	constructor() {}	
+	constructor(private dashboardService: DashboardService) {}	
 
 	ngOnInit() {}
 
@@ -38,15 +39,24 @@ export class TeamMembersComponent implements OnInit {
 		this.teamMembersGrid.instance.searchByText(this.searchText);
 	}
 
-	editRow(row) {
-		this.editDataRow = row;
+	editRow(memberData: any) {
+		this.editDataRow = memberData;
+		this.emailNotify = memberData.data.emailOn;
 		//this.subGridEditClicked.emit(this.teamId);
 		this.teamMembers.forEach(teamMember => { teamMember.editMode = false});
 		this.teamMembersGrid.instance.cancelEditData();
-		this.teamMembersGrid.instance.editRow(row.rowIndex);
-		this.saveTeamMember = row.data
-		this.isEdit = true;
-		row.data.editMode = true;
+		this.teamMembersGrid.instance.editRow(memberData.rowIndex);
+		memberData.data.editMode = true;
+	}
+
+	saveMemberChanges(memberData: any) {
+		memberData.row.data.emailOn = this.emailNotify;
+		//**** once API is ready call API and If update successful -
+		memberData.data.role = memberData.row.data.role;
+		memberData.data.emailOn = memberData.row.data.emailOn;
+		memberData.data.level = memberData.row.data.level;
+		memberData.data.editMode = false;
+		this.teamMembersGrid.instance.cancelEditData();
 	}
 
 	gridOnCellPrepared(e) {
@@ -69,13 +79,32 @@ export class TeamMembersComponent implements OnInit {
 		}  
 	}
 
-	saveChanges(row) {
-
+	emailtoggle(e) {
+		this.emailNotify = e.checked;
 	}
 	
 	cancelChanges(row) {
 		row.data.editMode = false;
 		this.teamMembersGrid.instance.cancelEditData();
+	}
+
+
+	removeTeamMember(member: TeamMember) {
+		this.memberIds = [];
+		this.memberIds.push(member.memberId);
+
+		this.dashboardService.deleteTeamMembers(this.memberIds).subscribe(
+      (res:any) => {
+        if (res.success) {
+            console.log(`Team member deleted: ${member.memberId}`);
+						const removeIndex: number[] =[];
+						this.memberIds.forEach(memberId => {
+							removeIndex.push(this.teamMembers.findIndex(member => memberId == member.memberId));
+						});
+						removeIndex.forEach(index => this.teamMembers.splice(index, 1));
+        }
+      }
+    );
 	}
 
 	doSomethingForNow(data) {}
