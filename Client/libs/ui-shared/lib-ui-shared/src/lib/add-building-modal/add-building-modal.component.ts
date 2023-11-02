5adc1a4212d0b9461b92623de42c0db66a1f7fc4
Harika Myneni <hmyneni@costargroup.com>
2023-11-02 10:15:21 -0400

    Merged PR 11893: 130470-UI -Wire-Add-Building-DropDowns
    130470-UI -Wire-Add-Building-DropDowns
    
    Related work items: #130470

--- Client/libs/ui-shared/lib-ui-shared/src/lib/add-building-modal/add-building-modal.component.ts
+++ Client/libs/ui-shared/lib-ui-shared/src/lib/add-building-modal/add-building-modal.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Service } from './add-building-modal.service';
 import { DxFormComponent } from 'devextreme-angular';
+import { FormWizardService } from '@micro-components/services/form-wizard.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'mango-add-building-modal',
@@ -15,22 +17,23 @@ export class AddBuildingModalComponent implements OnInit{
   
   @ViewChild(DxFormComponent) form: DxFormComponent;
 
+  enableStateTextBox: boolean = false;
 
-  positions: string[];
-
-  states: string[];
-
-  portfolios: string[];
-
-  countries: string[];
+  public countryDropdownItem: any = [];
+  public portfolioDropdownItem: any = [];
+  public templateDropdownItem: any = [];
+  public subGroupDropdownItem: any = [];
+  public hierarchyDropdownItem: any = [];
+  public stateDropdownItem: any = [];
 
   constructor(
     public service: Service,
-    public dialogRef: MatDialogRef<AddBuildingModalComponent>
+    public dialogRef: MatDialogRef<AddBuildingModalComponent>,
+    private formWizardService: FormWizardService
+    
   ) {
-    this.states = service.getStates();
-    this.portfolios = service.getPortfolios();
-    this.countries = service.getCountries();
+    this.onCountryChanged = this.onCountryChanged.bind(this);
+    this.onPortFolioValueChanged = this.onPortFolioValueChanged.bind(this);
    }
 
   ngOnInit(): void {
@@ -39,7 +42,45 @@ export class AddBuildingModalComponent implements OnInit{
         this.close();
       }
     })
-      
+    this.getDropdownData();
+  }
+
+  public getDropdownData() {
+    let observableList;
+    observableList = forkJoin({
+        templateDropdownItem: this.formWizardService.getRenderSelect("", 18),
+        countryDropdownItem: this.formWizardService.getRenderSelect("0", 16),
+        portfolioDropdownItem: this.formWizardService.getRenderSelect("", 62),
+        stateDropDownItem: this.formWizardService.getRenderSelect("United States", 17),
+      });
+
+      observableList.subscribe((data: any) => {
+      this.countryDropdownItem = data.countryDropdownItem.data;
+      this.templateDropdownItem = data.templateDropdownItem.data.map(projectTemplateName => projectTemplateName.ProjectTemplateName);
+      this.portfolioDropdownItem = data.portfolioDropdownItem.data;
+      this.stateDropdownItem = data.stateDropDownItem.data;
+    });
+  }
+
+  onCountryChanged(e:any) {
+    if (e.value == 225) {
+      this.enableStateTextBox = false;
+    } else {
+      this.enableStateTextBox = true;
+    }
+  }
+
+  onPortFolioValueChanged(e:any) {
+    this.formWizardService.getRenderSelect(e.value, 57).subscribe((data) => {
+      this.templateDropdownItem = data.data;
+    
+    })
+    this.formWizardService.getRenderSelect(e.value, 9).subscribe((data) => {
+      this.subGroupDropdownItem = data.data;
+    })
+    this.formWizardService.getRenderSelect(e.value, 10).subscribe((data) => {
+      this.hierarchyDropdownItem = data.data;
+    })
   }
 
   public close() {
