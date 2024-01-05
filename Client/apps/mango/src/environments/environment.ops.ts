8bd07c0a24c25509abf6a5f2188e2280ee0fdc2b
Filipe Catarcione <fcatarcione@costargroup.com>
2024-01-05 14:56:35 -0500

    Merged PR 12529: Fix env file for ops build
    Fix env file for ops build
    
    Related work items: #132442

--- Client/apps/mango/src/environments/environment.ops.ts
+++ Client/apps/mango/src/environments/environment.ops.ts
@@ -4,38 +4,40 @@ class EnvironmentsCrem implements Environment {
   production = false;
   name = 'OPS';
   appUrls = {
-    accounting: 'http://service2.ops.corp.virtualpremise.com:8094/accountmanagement/api/accountmanagement',
-    batchAccounting: 'http://service2.ops.corp.virtualpremise.com:8090/batchaccounting',
-    discountRateProfiles: 'http://service2.ops.corp.virtualpremise.com:8094/accountingprofiles/api',
-    bookmarks: 'http://service2.ops.corp.virtualpremise.com:8094/bookmarks/api/bookmarks',
-    listpages: 'http://service2.ops.corp.virtualpremise.com:8094/listpages/api/listpage/',
-    financials:  'http://service2.ops.corp.virtualpremise.com:8094/financials/api/',  
-    dashboards: 'http://service2.ops.corp.virtualpremise.com:8094/dashboards/api/',
-    identity: 'https://identity.tsm.crem.aws.dshrp.com/api',
-    authentication: 'https://authentication.tsm.crem.aws.dshrp.com/api/',
-    authorization: 'http://172.20.9.224:30055/api/',
-    reports: 'http://service2.ops.corp.virtualpremise.com:8090/reports/api/',
-    taskApproval: '', // For local testing.
-    userMaintenance: 'http://service2.ops.corp.virtualpremise.com:8090/UserMaintenance/api/',
-    portfolioMaintenance: 'http://service2.ops.corp.virtualpremise.com:8090/portfolioMaintenance/api/',
-    objectMaintenance: 'http://service2.ops.corp.virtualpremise.com:8090/objectMaintenance/api/',
-    groupMaintenance: 'http://service2.ops.corp.virtualpremise.com:8090/groupMaintenance/api/',
-    alertsRules: 'http://service2.ops.corp.virtualpremise.com:8090/Alerts',
-    alerts: 'http://service2.ops.corp.virtualpremise.com:8090/Alerts',
-    leftNav: 'http://service2.ops.corp.virtualpremise.com:8090/leftNav/api/',
-    quickSearch: 'http://service2.ops.corp.virtualpremise.com:8090/QuickSearch/api',
-    userService: 'http://service2.ops.corp.virtualpremise.com:8090/UserService/api',
-    formWizard: 'http://service2.ops.corp.virtualpremise.com:8090/FormsEngine/api/',
-    objectActions: 'http://service2.ops.corp.virtualpremise.com:8090/objectActions/api/',
-    header: 'http://service2.ops.corp.virtualpremise.com:8090/Header/api/',
-    inAppDisclosure: 'http://service2.ops.corp.virtualpremise.com:8090/AccountingService/api/',
-    projects: 'http://172.20.9.224:30057/api/projects/',
-    accountingSummary:"http://service2.ops.corp.virtualpremise.com:8090/AccountingSummary/api/AccountingSummary/",
-    dataSetDictionary: '',
+    accounting: 'http://mangospa.ops.corp.virtualpremise.com:30080/accountmanagement/api',
+    batchAccounting: 'http://mangospa.ops.corp.virtualpremise.com:30080/batchAccounting/api',
+    discountRateProfiles: 'http://mangospa.ops.corp.virtualpremise.com:30080/accountingprofiles/api',
+    bookmarks: 'http://mangospa.ops.corp.virtualpremise.com:30080/bookmarks/api/',
+    listpages: 'http://mangospa.ops.corp.virtualpremise.com:30080/listpages/api/listpage/',
+    financials: 'http://mangospa.ops.corp.virtualpremise.com:30080/financials/api/',
+    dashboards: 'http://mangospa.ops.corp.virtualpremise.com:30080/dashboards/api/',
+    identity: 'https://identity.ops.crem.aws.dshrp.com/api',
+    authentication: 'https://authentication.ops.crem.aws.dshrp.com/api/',
+    authorization: 'http://mangospa.ops.corp.virtualpremise.com:30080/authorization/api/',
+    taskApproval: '', 
+    reports: 'http://mangospa.ops.corp.virtualpremise.com:30080/reports/api/',
+    userMaintenance: 'http://mangospa.ops.corp.virtualpremise.com:30080/userMaintenance/api/',
+    portfolioMaintenance: 'http://mangospa.ops.corp.virtualpremise.com:30080/portfolioMaintenance/api/',
+    objectMaintenance: 'http://mangospa.ops.corp.virtualpremise.com:30080/objectMaintenance/api/',
+    groupMaintenance: 'http://mangospa.ops.corp.virtualpremise.com:30080/groupMaintenance/api/',
+    alertsRules: 'http://mangospa.ops.corp.virtualpremise.com:30080/Alerts/api/Alerts',
+    alerts: 'http://mangospa.ops.corp.virtualpremise.com:30080/Alerts/api/Alerts',
+    accountingService: 'http://mangospa.ops.corp.virtualpremise.com:30080/AccountingDashboard/api/Accounting',
+    leftNav: 'http://mangospa.ops.corp.virtualpremise.com:30080/leftNav/api/',
+    quickSearch: 'http://mangospa.ops.corp.virtualpremise.com:30080/quickSearch/api',
+    userService: 'http://mangospa.ops.corp.virtualpremise.com:30080/userService/api',
+    formWizard: 'http://mangospa.ops.corp.virtualpremise.com:30080/FormsEngine/api/',
+    objectActions: 'http://mangospa.ops.corp.virtualpremise.com:30080/objectActions/api/',
+    header: 'http://mangospa.ops.corp.virtualpremise.com:30080/header/api/',
+    inAppDisclosure: 'http://mangospa.ops.corp.virtualpremise.com:30080/AccountingDashboard/api/',
+    accountingSummary:"http://mangospa.ops.corp.virtualpremise.com:30080/AccountingSummary/api/",
+    dataSetDictionary: 'http://mangospa.ops.corp.virtualpremise.com:30080/reports/api/',
+    projects: 'http://mangospa.ops.corp.virtualpremise.com:30080/projects/api/projects/',
   };
   isRestful = true;
   cremBaseUrl = 'http://[CLIENT].ops.corp.virtualpremise.com';
   CAUrl = 'https://client.tsm.crem.aws.dshrp.com/';
+  mangoSpaUrl = 'http://mangospa.ops.corp.virtualpremise.com:30080/';
 }
 
 export const environment = new EnvironmentsCrem();
