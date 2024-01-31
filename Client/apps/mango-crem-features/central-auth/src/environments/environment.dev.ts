e0571dcb735ffb93ecddbd01e57cec98166a91bb
Filipe Catarcione <fcatarcione@costargroup.com>
2024-01-31 09:13:31 -0500

    Merged PR 12772: Point CA spa and Mango spa to new CA URIs for backend services - DEV environment
    Related work items: #133176

--- Client/apps/mango-crem-features/central-auth/src/environments/environment.dev.ts
+++ Client/apps/mango-crem-features/central-auth/src/environments/environment.dev.ts
@@ -2,8 +2,8 @@ class EnvironmentsCrem {
   production = false;
   name = 'DEV';
   appUrls = {
-    identity: 'https://identity.dev.crem.aws.dshrp.com/api',
-    authentication: 'https://authentication.dev.crem.aws.dshrp.com/api',
+    identity: 'http://mangospa.dev.corp.virtualpremise.com:30080/identity/api',
+    authentication: 'http://mangospa.dev.corp.virtualpremise.com:30080/authentication/api',
     bookmarks: '/v06/WebServices/Mango/Bookmarks/Bookmarks.asmx/',
     leftNav: '',
     dashboards: '',
@@ -18,7 +18,7 @@ class EnvironmentsCrem {
   isRestful: false;
   mangoSpaUrl = 'http://service2.dev.corp.virtualpremise.com:8097/'
   cremBaseUrl = 'http://[CLIENT].dev.corp.virtualpremise.com/';
-  CAUrl = 'https://client-alpha.dev.crem.aws.dshrp.com/';
+  CAUrl = 'http://login.dev.corp.virtualpremise.com:30080/';
 }
 
 export const environment = new EnvironmentsCrem();
