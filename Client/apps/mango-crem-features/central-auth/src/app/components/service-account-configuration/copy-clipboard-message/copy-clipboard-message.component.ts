fb7d6a2c4576e575938c63cb486be007511630ba
Li Liu <lliu@costargroup.com>
2024-02-22 12:29:37 -0500

    Merged PR 13013: 134372: Client Delivery - New CA Config - CA Configuration Page - Insert and retrieval of data
    134372: Client Delivery - New CA Config - CA Configuration Page - Insert and retrieval of data
    
    Another try
    
    Related work items: #134372

--- Client/apps/mango-crem-features/central-auth/src/app/components/service-account-configuration/copy-clipboard-message/copy-clipboard-message.component.ts
+++ Client/apps/mango-crem-features/central-auth/src/app/components/service-account-configuration/copy-clipboard-message/copy-clipboard-message.component.ts
@@ -24,13 +24,26 @@ export class CopyClipboardMessageComponent {
   copyClipboard() {
     try {
       navigator.clipboard.writeText(this.apiKey);
+      console.log('using navigator.clipboard.writeText');
     } catch (error) {
-      const textarea = document.createElement("textarea");
-      textarea.textContent = this.apiKey;
-      document.body.appendChild(textarea);
-      textarea.select();
-      document.execCommand("copy");
-      document.body.removeChild(textarea);
+      console.log('using document.execCommand');
+      const el = document.createElement('textarea');
+      el.value = this.apiKey;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      const selected =
+        document.getSelection().rangeCount > 0
+          ? document.getSelection().getRangeAt(0)
+          : false;
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+      }
     }
 
     this.info = "Copied to clipboard";
