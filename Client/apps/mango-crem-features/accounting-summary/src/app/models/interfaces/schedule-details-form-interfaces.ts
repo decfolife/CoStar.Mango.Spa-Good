af07b1ec394c45191a8965cc087c5a2ee2dcfbc6
Naveed Karimi <nkarimi@costargroup.com>
2025-09-18 16:35:39 -0400

    Merged PR 21233: Fix Ada issues on Batch Accounting and Accounting Summary pages
    Fix Ada issues on Batch Accounting and Accounting Summary pages
    
    Related work items: #153935

--- Client/apps/mango-crem-features/accounting-summary/src/app/models/interfaces/schedule-details-form-interfaces.ts
+++ Client/apps/mango-crem-features/accounting-summary/src/app/models/interfaces/schedule-details-form-interfaces.ts
@@ -28,3 +28,21 @@ export interface ScheduleDetailsFormData {
   termBegin: TermDateOption;
   termEnd: TermDateOption;
 }
+
+export interface ScheduleDetailsTermsInformation {
+  isDayOne: boolean;
+  termBegin: Date;
+  termEnd: Date;
+  termsInMonth: number;
+}
+
+export interface AccountingTerms {
+  leaseAbstractID: number;
+  termBegin: Date;
+  termEnd: Date;
+  termInDays: number;
+  termInMonths: number;
+  termInPeriods: number;
+  termInYears: number;
+  termString: string;
+}
--- Client/apps/mango-crem-features/accounting-summary/src/app/services/add-edit-schedule.service.ts
+++ Client/apps/mango-crem-features/accounting-summary/src/app/services/add-edit-schedule.service.ts
@@ -278,10 +278,9 @@ export class AddEditScheduleService extends EndpointService {
   }
 
   toShortDateString(dateInput: Date): string | null {
-    if (!dateInput) {
-      return null;
-    }
+    if (!dateInput) return null;
     const date = new Date(dateInput);
+    if (isNaN(date.getTime())) return null; // invalid date check
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
